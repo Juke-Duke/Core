486b20975c10dae891a6cdb141d40661cdf6bdbb
Youssef Elshabasy <youssefelshabasydev@gmail.com>
2025-02-19 06:15:03 -0500

    Implemented Deque

--- core/collections/Deque.h
+++ core/collections/Deque.h
@@ -20,6 +20,7 @@
 #define Deque(DequeElement) GENERIC(Deque, DequeElement)
 #define DequeDefault(DequeElement) GENERIC(DequeDefault, DequeElement)
 #define DequeCount(DequeElement) GENERIC(DequeCount, DequeElement)
+#define DequeResize(DequeElement) GENERIC(DequeResize, DequeElement)
 #define DequeFront(DequeElement) GENERIC(DequeFront, DequeElement)
 #define DequeBack(DequeElement) GENERIC(DequeBack, DequeElement)
 #define DequeQueueFront(DequeElement) GENERIC(DequeQueueFront, DequeElement)
@@ -47,6 +48,26 @@ static UInt DequeCount(DequeElement)(Deque(DequeElement) * deque) {
   return deque->count;
 }
 
+static void DequeResize(DequeElement)(Deque(DequeElement) * deque) {
+  var oldRing = deque->ring;
+
+  deque->ring = ArrayCreateWithCapacity(DequeElement)(ArrayCapacity(DequeElement)(&oldRing) * 2 + 8);
+
+  for (var i = 0; i < deque->count; ++i) {
+    ArraySetAt(DequeElement)(
+      &deque->ring,
+      i,
+      ArrayAt(DequeElement)(
+        &oldRing,
+        (deque->front + i) % ArrayCapacity(DequeElement)(&oldRing)
+      )
+    );
+  }
+
+  deque->front = 0;
+  ArrayDestroy(DequeElement)(oldRing);
+}
+
 static Option(DequeElement) DequeFront(DequeElement)(Deque(DequeElement) * deque) {
   if (deque->count == 0) {
     return OptionNone(DequeElement)();
@@ -60,55 +81,53 @@ static Option(DequeElement) DequeBack(DequeElement)(Deque(DequeElement) * deque)
     return OptionNone(DequeElement)();
   }
 
-  var back = (deque->front + deque->count - 1) % ArrayCapacity(DequeElement)(&deque->ring);
-
-  return OptionSome(DequeElement)(ArrayAt(DequeElement)(&deque->ring, back));
+  return OptionSome(DequeElement)(
+    ArrayAt(DequeElement)(&deque->ring, (deque->front + deque->count - 1) % ArrayCapacity(DequeElement)(&deque->ring))
+  );
 }
 
 static void DequeQueueFront(DequeElement)(Deque(DequeElement) * deque, DequeElement element) {
   if (deque->count == ArrayCapacity(DequeElement)(&deque->ring)) {
-    ArrayResize(DequeElement)(&deque->ring, deque->count * 2 + 8);
+    DequeResize(DequeElement)(deque);
   }
 
   deque->front = (deque->front - 1 + ArrayCapacity(DequeElement)(&deque->ring)) % ArrayCapacity(DequeElement)(&deque->ring);
   ArraySetAt(DequeElement)(&deque->ring, deque->front, element);
   ++deque->count;
-
-  // print the ring
-  for (var i = 0; i < ArrayCapacity(DequeElement)(&deque->ring); ++i) {
-    printf("%3d ", ArrayAt(DequeElement)(&deque->ring, i));
-  }
-  printf("\n");
 }
 
 static void DequeQueueBack(DequeElement)(Deque(DequeElement) * deque, DequeElement element) {
   if (deque->count == ArrayCapacity(DequeElement)(&deque->ring)) {
-    ArrayResize(DequeElement)(&deque->ring, deque->count * 2 + 8);
+    DequeResize(DequeElement)(deque);
   }
 
   var back = (deque->front + deque->count) % ArrayCapacity(DequeElement)(&deque->ring);
   ArraySetAt(DequeElement)(&deque->ring, back, element);
   ++deque->count;
-
-  for (var i = 0; i < ArrayCapacity(DequeElement)(&deque->ring); ++i) {
-    printf("%3d ", ArrayAt(DequeElement)(&deque->ring, i));
-  }
-  printf("\n");
 }
 
 static Option(DequeElement) DequeDequeueFront(DequeElement)(Deque(DequeElement) * deque) {
-  var front = DequeFront(DequeElement)(deque);
+  if (deque->count == 0) {
+    return OptionNone(DequeElement)();
+  }
 
+  var element = ArrayAt(DequeElement)(&deque->ring, deque->front);
   deque->front = (deque->front + 1) % ArrayCapacity(DequeElement)(&deque->ring);
   --deque->count;
 
-  return front;
+  return OptionSome(DequeElement)(element);
 }
 
 static Option(DequeElement) DequeDequeueBack(DequeElement)(Deque(DequeElement) * deque) {
-  var back = DequeBack(DequeElement)(deque);
+  if (deque->count == 0) {
+    return OptionNone(DequeElement)();
+  }
+
+  var back = (deque->front + deque->count - 1) % ArrayCapacity(DequeElement)(&deque->ring);
+  var element = ArrayAt(DequeElement)(&deque->ring, back);
   --deque->count;
-  return back;
+
+  return OptionSome(DequeElement)(element);
 }
 
 static void DequeDestroy(DequeElement)(Deque(DequeElement) deque) {
