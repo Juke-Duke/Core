d1b686d40ad30f1ee80d5fc7222a7fdad0372f43
Youssef Elshabasy <youssefelshabasydev@gmail.com>
2026-02-17 18:33:02 -0500

    Refactor implement_i macro to ensure static keyword is correctly placed for function declaration

--- core/Interface.h
+++ core/Interface.h
@@ -12,13 +12,13 @@
 
 #define implement(...) implement_i(__VA_ARGS__)
 
-#define implement_i(interfaceName, typeName, ...)                          \
-  interfaceName* typeName##_as_##interfaceName(typeName self) {            \
-    static typeof(*(interfaceName){}.interface) interface = {__VA_ARGS__}; \
-    auto obj = (interfaceName*)malloc(sizeof(void*) + sizeof(typeName));   \
-    obj->interface = &interface;                                           \
-    memcpy(obj->self, &self, sizeof(typeName));                            \
-    return obj;                                                            \
+#define implement_i(interfaceName, typeName, ...)                                                                     \
+  static interfaceName* typeName##_as_##interfaceName(typeName self) {                                                \
+    static typeof(*(interfaceName){}.interface) interface = {__VA_ARGS__};                                            \
+    auto obj                                              = (interfaceName*)malloc(sizeof(void*) + sizeof(typeName)); \
+    obj->interface                                        = &interface;                                               \
+    memcpy(obj->self, &self, sizeof(typeName));                                                                       \
+    return obj;                                                                                                       \
   }
 
 #define as(typeName, interfaceName) typeName##_as_##interfaceName
